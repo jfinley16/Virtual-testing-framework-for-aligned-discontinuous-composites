29829854476eaea85311ce4b75191a6592f6e15e
James M. Finley <42179374+jfinley16@users.noreply.github.com>
2018-10-24 15:36:11 +0100

    Update redblue.m
    this file was downloaded from MathWorks and was sued as an easy way to create a red and blue colormap

--- redblue.m
+++ redblue.m
@@ -3,7 +3,7 @@ function c = redblue(m)
 %   REDBLUE(M), is an M-by-3 matrix that defines a colormap.
 %   The colors begin with bright blue, range through shades of
 %   blue to white, and then through shades of red to bright red.
-%   REDBLUE, by itself, is the same length as the current figure's
+%   REDBLUE, by itself, is the same length as the current figure's 
 %   colormap. If no figure exists, MATLAB creates one.
 %
 %   For example, to reset the colormap of the current figure:
